7d843a04d5d89b6cfd21ae3ae58e625a5ef75f15
Oscar Javier Pazos Mogollón <turokoscar@gmail.com>
2026-04-12 14:00:20 -0500

    Eliminacion de archivos
    Se eliminan archivos no utilizados

--- testdb.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2/promise');
-async function test() {
-  const conn = await mysql.createConnection('mysql://root:12345678@localhost:3306/BD_DOCUFISE_DEV');
-  const [rows] = await conn.execute('SELECT ide_usuario, txt_nombreUsuario, flg_activo FROM usuario');
-  console.log(rows);
-  conn.end();
-}
-test().catch(console.error);
